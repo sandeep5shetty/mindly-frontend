695c745d570f51a5af62bc31a1852ff2bbb8bb69
Sandeep Shetty <sandeepshettysigadalu@gmail.com>
2025-10-21 23:07:41 +0530

    added button Variants

--- src/components/AuthPage.tsx
+++ src/components/AuthPage.tsx
@@ -4,7 +4,7 @@ export const AuthPage = () => {
   return (
     <>
       <div className="flex h-screen items-center justify-center bg-[#1a1a1a]">
-        <Button variant={'secondary'} size={'sm'} className="cursor-pointer">
+        <Button variant={'destructive'} size={'sm'} className="cursor-pointer">
           Click Me
         </Button>
       </div>
